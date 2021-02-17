0fa88a2bdb35763be7eabc1f905fa7ebf0840cae
Alex Brand <47761373+pel-ex@users.noreply.github.com>
2021-02-17 12:42:19 +0000

    Update endpoint.php

--- endpoint.php
+++ endpoint.php
@@ -2,15 +2,11 @@
     if($json = json_decode(file_get_contents("php://input"), true)) {
         print_r($json);
         $data = $json;
-        //logging to file
-        $log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
-        "Attempt: ".($result[0]['success']=='1'?'Success':'Failed').PHP_EOL.
-        "Data: ".$json.PHP_EOL.
-        "-------------------------".PHP_EOL;
-        //Save string to log, use FILE_APPEND to append.
-        file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
     } else {
         print_r($_POST);
         $data = $_POST;
     }
-?>
+    $fWrite = fopen("log.txt","a");
+    $wrote = fwrite($fWrite, var_dump($_POST));
+    fclose($fWrite);
+?> 
