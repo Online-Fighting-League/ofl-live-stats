0aecc2c0208374562d2899fc7af8c8f43d31cfc6
Alex Brand <47761373+pel-ex@users.noreply.github.com>
2021-02-25 10:05:13 +0000

    Update endpoint.php

--- endpoint.php
+++ endpoint.php
@@ -1,12 +1,24 @@
 <?php
-    if($json = json_decode(file_get_contents("php://input"), true)) {
-        print_r($json);
-        $data = $json;
-    } else {
-        print_r($_POST);
-        $data = $_POST;
+    $heardData = file_get_contents("php://input");
+    if(!json_decode($heardData, true)) {
+        // is not valid json
+        return http_response_code(400);
     }
-    $fWrite = fopen("log.txt","a");
-    $wrote = fwrite($fWrite, var_dump($data));
-    fclose($fWrite);
-?> 
+
+    $conn = new mysqli('{servername}', '{username}', '{password}', '{dbname}');
+
+    // check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // construct query
+    $sql = "INSERT INTO loggy (logWhen, logWhat) VALUES (now(), '$heardData')";
+    if ($conn->query($sql) === TRUE) {
+        return http_response_code(201);
+    } else {
+        print "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
